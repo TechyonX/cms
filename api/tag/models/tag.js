b85f58505137094fa7cfd2512ff117eb97ce91d5
Bilguun Ochirbat <bilguun0203@gmail.com>
2020-06-26 20:13:51 +0800

    automatic slug  generation

--- api/tag/models/tag.js
+++ api/tag/models/tag.js
@@ -1,8 +1,50 @@
 'use strict';
 
 /**
+ * Бичвэрийг хаягийн мөрөнд тохиромжтой хэлбэрт оруулах функц
+ * @param {string} text - slug болгох бичвэр
+ * @returns {string}
+ */
+const apply_slugify = text => {
+    const slugify = require("slugify");
+    
+    slugify.extend({
+      ж: "j",
+      Ж: "J",
+      ө: "o",
+      Ө: "ө",
+      ү: "u",
+      Ү: "U",
+      й: "i",
+      Й: "I",
+      ц: "ts",
+      Ц: "Ts",
+      ъ: "",
+      Ъ: "",
+    });
+
+    return slugify(text, {
+          lower: true,
+          strict: true,
+        });
+};
+
+/**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async beforeCreate(data) {
+      if (data.name) {
+        data.slug = apply_slugify(data.name);
+      }
+    },
+    async beforeUpdate(params, data) {
+      if (data.name) {
+        data.slug = apply_slugify(data.name);
+      }
+    },
+  },
+};
