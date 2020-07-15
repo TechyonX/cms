945afae47f78b5f2ccf1ccefbdb8977cdc3e72a4
Bilguun Ochirbat <bilguun0203@gmail.com>
2020-07-15 12:56:48 +0800

    Automatic slug generation & Post status (#3)
    * automatic slug  generation
    
    * added fields
    
    * ноорог нийтлэлийн боломж (publish: false)
    
    * Ноорог нийтлэлийг count хүсэлтэнд оруулж тоолохгүй
    
    * changed post status  data type to enum from bool
    
    * нийтлэл бичсэн хүнд тухайн нийтлэлийн эрхийг өгнө
    
    * Revert "нийтлэл бичсэн хүнд тухайн нийтлэлийн эрхийг өгнө"
    
    This reverts commit f6fe433efb0df6153e7b313fcb40f63dd90d3026.

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -1,8 +1,52 @@
 'use strict';
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
+  async find(ctx) {
+    let entities;
+
+    ctx.query = {
+      ...ctx.query,
+      status: 'published',
+    };
+
+    if (ctx.query._q) {
+      entities = await strapi.services.post.search(ctx.query);
+    } else {
+      entities = await strapi.services.post.find(ctx.query);
+    }
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.post })
+    );
+  },
+
+  /**
+   * Count records.
+   *
+   * @return {Number}
+   */
+
+  count(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      status: 'published',
+    };
+
+    if (ctx.query._q) {
+      return strapi.services.post.countSearch(ctx.query);
+    }
+    return strapi.services.post.count(ctx.query);
+  },
+};
